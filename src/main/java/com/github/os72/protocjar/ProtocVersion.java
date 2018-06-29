03eeaba1284ae6ce216db3d7ada71c88164fa437
os72 <osuciu@yahoo.com>
2018-06-28 19:19:42 -0700

    switch to 3.6.0

--- src/main/java/com/github/os72/protocjar/ProtocVersion.java
+++ src/main/java/com/github/os72/protocjar/ProtocVersion.java
@@ -20,7 +20,7 @@ import java.util.Map;
 
 public class ProtocVersion
 {
-	public static final ProtocVersion PROTOC_VERSION = new ProtocVersion(null, null, "3.5.1");
+	public static final ProtocVersion PROTOC_VERSION = new ProtocVersion(null, null, "3.6.0");
 
 	public static ProtocVersion getVersion(String spec) {
 		if (!spec.startsWith("-v")) return null;
